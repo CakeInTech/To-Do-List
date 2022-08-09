12cb8ef6dce57fa81345e7a04f55e11fc71a39cc
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-08-09 20:06:41 +0200

    Complete Exercise

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
+// import Icon from './icon.png';
 
  function component() {
    const element = document.createElement('div');
   const btn = document.createElement('button');
 
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.classList.add('hello');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
   element.appendChild(btn);
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
    return element;
  }
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,21 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
- entry: {
-   index: './src/index.js',
-   print: './src/print.js',
- },
- plugins: [
-  new HtmlWebpackPlugin({
-    title: 'Output Management',
-    template: './src/template.html',
-  }),
-],
-devServer: {
-  static: './dist',
-},
+  entry: './src/index.js',
   output: {
-   filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -31,13 +19,13 @@ devServer: {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/template.html',
+
+    }),
+  ],
 };
